08668b99cfeefe14f27d9de5b23367316af765ad
spilmont <dd_spilmont@hotmail.fr>
2018-12-02 21:50:46 +0100

    a faire: - high score- fluidité cible

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var score =0;
-var seconde=30;
+var highScore =[];
+var score = 0;
+var seconde = 30;
 var temps;
-var x=0;
-var y=0;
+var x = 0;
+var y = 0;
 var start = document.createElement("span");
 start.innerHTML = 'START';
 start.id = 'start';
-
 
 
 document.getElementById('plateau').appendChild(start);
@@ -28,7 +28,7 @@ document.getElementById("start").addEventListener("click", function () {
     start.style.display = "none";
     document.getElementById('titre').style.visibility = "hidden";
     document.getElementById('me').style.visibility = "hidden";
-    temps =setTimeout(CaR,1000);
+     setTimeout(CaR, 1000);
 
 });
 
@@ -41,34 +41,59 @@ document.getElementById('plateau').addEventListener("click", function () {
 
 document.getElementById('chauvesouris').addEventListener("click", function () {
 
-    if(document.getElementById('start').style.display == 'none'){
+    if (document.getElementById('start').style.display == 'none') {
         score++;
     }
 
-    document.getElementById('score').innerHTML= `score: ${score}`;
+    document.getElementById('score').innerHTML = `score: ${score}`;
 
 })
 
-function CaR(){
+function CaR() {
     seconde--;
     document.getElementById('seconde').innerHTML = `seconde: ${seconde}`;
-    setTimeout(CaR,1000);
-
-}
-if(seconde === 0) {
-    seconde =0;
+     temps = setTimeout(CaR, 1000);
+if (seconde === 0) {
+    seconde = 0;
+    highScore.sort();
+    highScore.reverse();
+    highScore.push(score+"<br>");
     document.getElementById('ecranScore').style.display = "block";
     document.getElementById('plateau').style.display = "none";
-    clearTimeout(CaR);
+    for (let i=0;i<highScore.length;i++){
+    document.getElementById('highScore').innerHTML = highScore;
+}
+    clearTimeout(temps);
+    console.log(highScore);
+}
 }
 
-function deplacersCS(){
-    if((document.getElementById('start').style.display == 'none')){
-    x++;
-    y++;
-    document.getElementById('chauvesouris').style.top = (Math.random()*600+y)+"px";
-    document.getElementById('chauvesouris').style.left = (Math.random()*1600+x)+"px";
-}
-    setTimeout(deplacersCS,1000);
+function deplacersCS() {
+    if ((document.getElementById('start').style.display == 'none')) {
+        x++;
+        y++;
+        document.getElementById('chauvesouris').style.top = (Math.random() * 600 + y) + "px";
+        document.getElementById('chauvesouris').style.left = (Math.random() * 1600 + x) + "px";
+    }
+    setTimeout(deplacersCS, 1000);
 }
 deplacersCS();
+
+function rejouer() {
+    temps = setTimeout(CaR,1000);
+    score =0;
+    seconde =30;
+    x=0;
+    y=0;
+    document.getElementById('score').innerHTML ="score: 0";
+    document.getElementById('seconde').innerHTML ="seconde: 30";
+    document.getElementById('chauvesouris').style.top=35+"vh";
+    document.getElementById('chauvesouris').style.top=38+"vw";
+    document.getElementById('plateau').style.display = "block";
+    document.getElementById('ecranScore').style.display = "none";
+
+}
+document.getElementById('rejouer').addEventListener('click',function () {
+    rejouer();
+
+});
